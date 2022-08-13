1a7c40e92974c7423bc44db3b48c3329ba8ce9d8
Ihab El amrani <iel-amra@z4r3p5.42lyon.fr>
2022-08-13 20:57:47 +0200

    Solved

--- src/line_algo.c
+++ src/line_algo.c
@@ -1,47 +1,64 @@
-void	get_best_sa(t_stacks *stacks, int nb, int **line, char *best_sa)
+void	get_best_sa(t_stacks *stacks, int nb, int **line, int *best_sa)
 {
-	char	i;
+	int	i;
 	int	best_score;
 	int	score;
 	int	*new_line;
 
-	*best_sa = 0;
+	//*best_sa = -1;
 	i = 0;
 	best_score = -3;
-	while (i < ft_pow(2, nb - 1) && (i == 0 || line))
-	{
-		move_sa_byte(stacks, nb, i, 0);
-		score = score_line(stacks, nb, &new_line);
-		if (score > best_score || score == -2)
+	*line = (void *) 0;
+	while (i < ft_pow(2, nb - 1) && (i == 0 || *line))
+	{	
+		move_sa_binary(stacks, nb, i, 0);
+		score = score_line(stacks, nb, &new_line);	
+		if ((score > best_score && (true_bits(i) < true_bits(*best_sa) || best_score < 0)) || score == -2)
 		{
 			free(*line);
 			*line = new_line;
 			best_score = score;
 			*best_sa = i;
 		}
-		free(new_line);
-		move_sa_byte(stacks, nb, i, 1);
+		move_sa_binary(stacks, nb, i, 1);
 		i++;
 	}
 }
 
-void	move_sa_byte(t_stacks *stacks, int nb, char byte, int mode)
+int	true_bits(int x)
 {
-	char	i;
+	unsigned long	i;
+	int	nb;
+
+	nb = 0;
+	i = 0;
+	while (i < sizeof(int))
+	{
+		if (x >> i & 1)
+			nb++;
+		i++;
+	}
+	return (nb);
+}
+
+void	move_sa_binary(t_stacks *stacks, int nb, int byte, int mode)
+{
+	int	i;
 
 	i = 0;
-	while (i < nb - 1)
+	while (i < nb)
 	{
 		if (byte >> i & 1 && mode == 0)
 			move(stacks, SA, 0);
 		move(stacks, RA, 0);
 		i++;
 	}
-	while (i > 0)
+	i--;
+	while (i >= 0)
 	{
+		move(stacks, RRA, 0);
 		if (byte >> i & 1 && mode == 1)
 			move(stacks, SA, 0);
-		move(stacks, RRA, 0);
 		i--;
 	}
 }
@@ -64,9 +81,21 @@ int	score_line(t_stacks *stacks, int nb, int **line)
 	if (score == -2)
 	{
 		free(*line);
-		*line = (void *);
+		*line = (void *) 0;
 	}
 	return (score);
+}
+
+int	*tab_int_copy(int *line, int len)
+{
+	int	*cpy;
+
+	cpy = malloc(len * sizeof(int));
+	if (!cpy)
+		return ((void *) 0);
+	while (--len >= 0)
+		cpy[len] = line[len];
+	return (cpy);
 }
 
 int	score_recur(t_stacks *stacks, int step, int nb, int **line)
@@ -84,15 +113,83 @@ int	score_recur(t_stacks *stacks, int step, int nb, int **line)
 	new = tab_int_copy(*line, nb + 1);
 	if (!new)
 		return (-2);
-	(*new)[i] = int_content(stacks->a);
-	move(stacks, RA);
+	new[i] = int_content(stacks->a);
+	move(stacks, RA, 0);
 	score_tab[0] = -3;
-	if (!*line[0] == -1 || (*line)[i] < (*new)[i])
-		score_tab[0] = score_recur(stacks, step - 1, nb, new);
+	if ((*line)[0] == -1 || new[i - 1] < new[i])
+		score_tab[0] = score_recur(stacks, step - 1, nb, &new);
 	score_tab[1] = score_recur(stacks, step - 1, nb, line);
-	move(stacks, RRA);
+	move(stacks, RRA, 0);
 	score = choose_best_score(score_tab, line, &new);
 	return (score);
+}
+
+int	is_in_tab(int *tab, int nb, int len)
+{
+	while (len-- > 0)
+	{
+		if (*tab++ == nb)
+			return (1);
+	}
+	return (0);
+}
+
+void	print_tab(int *tab, int nb)
+{
+	int	i = 0;
+
+	while (i < nb)
+	{
+		ft_printf("%i ", tab[i]);
+		i++;
+	}
+	ft_printf("\n");
+}
+
+int	stop_recur(t_stacks *stacks, int **line, int nb)
+{
+	int	i;
+	int	sep;
+
+	//static int	compteur = 0;
+
+	i = 0;
+	while (i++ < nb)
+		move(stacks, RRA, 0);
+	sep = 0;
+	while ((*line)[sep] != -1)
+		sep++;
+	(*line)[nb] = sep;
+	i = 0;
+	while (i++ < nb)
+	{
+		if (!is_in_tab(*line, int_content(stacks->a), nb))
+			(*line)[sep++] = int_content(stacks->a);
+		move(stacks, RA, 0);
+	}
+/*
+	if (score_tab(*line, nb) >= 0) 
+	{
+		ft_printf("%i\n", compteur);
+	   	print_tab(*line, nb);
+	}
+	compteur++;
+*/
+	return (score_tab(*line, nb));
+}
+
+int	score_tab(int *line, int nb)
+{
+	int	i;
+
+	i = line[nb];
+	while (i < nb - 1)
+	{
+		if (line[i] < line[i + 1])
+			return (-1);
+		i++;
+	}
+	return (line[nb]);
 }
 
 int	choose_best_score(int *score, int **line, int **new)
@@ -107,6 +204,5 @@ int	choose_best_score(int *score, int **line, int **new)
 		return (-2);
 	if (score[0] > score[1])
 		return (score[0]);
-	return (score[1]);
-	
+	return (score[1]);	
 }
--- src/solve.c
+++ src/solve.c
@@ -6,7 +6,7 @@
 /*   By: iel-amra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 16:42:50 by iel-amra          #+#    #+#             */
-/*   Updated: 2022/08/10 17:57:16 by iel-amra         ###   ########.fr       */
+/*   Updated: 2022/08/13 20:55:46 by iel-amra         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,17 +51,101 @@ static int	get_nb_max(t_list *stack)
 	return (nb_max);
 }	
 
+void	line_first_step(t_stacks *stacks, int nb, int *line, int best_sa)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = line[nb];
+	while (j < nb)
+	{
+		if (best_sa >> i & 1)
+			move(stacks, SA, 1);
+		if (line[j] == int_content(stacks->a))
+		{
+			move(stacks, PB, 1);
+			j++;
+		}
+		else if (j < nb)
+			move(stacks, RA, 1);
+		i++;
+	}
+}
+
+void	line_prepare_second(t_stacks *stacks, int nb, int *line)
+{
+	int	sep;
+	int last;
+
+	sep = line[nb];
+	if (sep == nb)
+		return ;
+	last = int_content(ft_lstlast(stacks->a));
+	while (is_in_tab(line, last, sep) && last > line[nb - 1] && int_content(stacks->a) != line[0])
+	{
+		move(stacks, RRA, 1);
+		last = int_content(ft_lstlast(stacks->a));
+	}
+}
+
+void	line_second_step(t_stacks *stacks, int nb, int *line)
+{
+	int	smallest;
+
+	smallest = line[0];
+	if (line[nb - 1] < smallest)
+		smallest = line [nb - 1];
+		if (stacks->b && (!stacks->a || !is_in_tab(line, int_content(stacks->a), nb) || int_content(stacks->b) < int_content(stacks->a)))
+				move(stacks, PA, 1);
+		move(stacks, RA, 1);
+	while (!stacks->a || stacks->b || (is_in_tab(line, int_content(stacks->a), nb) && int_content(stacks->a) != smallest))
+	{
+		if (stacks->b && (!stacks->a || !is_in_tab(line, int_content(stacks->a), nb) 
+				|| int_content(stacks->a) == smallest || int_content(stacks->b) < int_content(stacks->a)))
+				move(stacks, PA, 1);
+		move(stacks, RA, 1);
+	}
+}
+
+int	line_solve(t_stacks *stacks, int nb)
+{
+	int	best_sa;
+	int	*line;
+
+	get_best_sa(stacks, nb, &line, &best_sa);
+	if (!line)
+		return (1);
+	//print_tab(line, nb);
+	//put_stacks(stacks);
+	line_first_step(stacks, nb, line, best_sa);
+	//put_stacks(stacks);
+	line_prepare_second(stacks, nb, line);
+	//put_stacks(stacks);
+	line_second_step(stacks, nb, line);
+	//put_stacks(stacks);
+	free(line);
+	return (0);
+}
+
 int	solve(t_stacks *stacks)
 {
 	int	nb_bit_max;
 	int	i;
 	int	nb_max;
 	char	*brute;
+	int	nb_line;
+	int	len_line;	
 
 	nb_max = get_nb_max(stacks->a);
 	nb_bit_max = get_nb_bit(nb_max);
 	
-	if (nb_max + 1 <= 8)
+/*
+	line_solve(stacks, nb_max + 1, line, best_sa);
+	return (0);
+*/
+	
+	if (nb_max + 1 < 8)
 	{
 		brute = brute_force(nb_max + 1, 0);
 		if (!brute)
@@ -76,7 +160,8 @@ int	solve(t_stacks *stacks)
 		free(brute);
 		return (0);
 	}
-	i = 0; //nb_bit_max % 4;
+
+	i = nb_bit_max % 4;
 	while (i < nb_bit_max)
 	{
 		radix(i, stacks, nb_max + 1, A_TO_B);
@@ -84,5 +169,12 @@ int	solve(t_stacks *stacks)
 			B_TO_A | LAST * (i + 4 >= nb_bit_max));	
 		i += 4;
 	}
+	len_line = ft_pow(2, nb_bit_max % 4);
+	nb_line = (nb_max + 1) / len_line; 
+	i = 0;
+	while (i++ < nb_line)
+		line_solve(stacks, len_line);
+	if ((nb_max + 1) % len_line)
+		line_solve(stacks, (nb_max + 1) % len_line);
 	return (0);
 }
